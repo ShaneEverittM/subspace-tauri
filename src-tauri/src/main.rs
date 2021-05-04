f17a23704febb3c2ef4a7714a5243dfb4f787559
ShaneEverittM <shane.everitt.murphy@gmail.com>
2021-05-04 00:09:22 -0700

    WIP: Implement simple example API.

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,9 +2,36 @@
     all(not(debug_assertions), target_os = "windows"),
     windows_subsystem = "windows"
 )]
+use basis::Matrix;
+use serde::Serialize;
+use std::convert::TryInto;
+use std::string::ToString;
+use thiserror::Error;
+
+#[derive(Debug, Error, Serialize)]
+pub enum ApiError {
+    #[error("{0}")]
+    InvalidInput(String),
+    #[error("Internal Error: {0}")]
+    BasisError(String),
+}
+
+impl From<anyhow::Error> for ApiError {
+    fn from(e: anyhow::Error) -> Self {
+        ApiError::BasisError(e.to_string())
+    }
+}
+
+#[tauri::command]
+pub fn add(v1: Vec<Vec<i32>>, v2: Vec<Vec<i32>>) -> Result<Vec<Vec<i32>>, ApiError> {
+    Matrix::add(&v2.try_into()?, &v1.try_into()?)
+        .map(|m| (*m).clone())
+        .map_err(|e| e.into())
+}
 
 fn main() {
     tauri::Builder::default()
+        .invoke_handler(tauri::generate_handler![add])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
