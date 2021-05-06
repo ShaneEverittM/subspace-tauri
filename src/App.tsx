93566cd86a9b2c4defbcbbce3d3c060e14cd72a1
ShaneEverittM <shane.everitt.murphy@gmail.com>
2021-05-05 21:41:32 -0700

    WIP: Fine-tune API, format is roughly done, will need to be expanded.
    Using serde_repr, we now have the ability to concretely map error types by enum discriminant across language boundaries :^)

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,20 @@
 import React, {useEffect, useState} from 'react';
-import logo from './logo.svg';
 import './App.css';
-import {invoke} from '@tauri-apps/api/tauri'
+import {handleError} from "./Error";
+import {add} from "./Arithmetic"
+
 
 function App() {
     let [sum, setSum] = useState(new Array<Array<number>>())
+
     useEffect(() => {
-        invoke<Array<Array<number>>>('add', {
-            v1: [[1, 2], [3, 4]],
-            v2: [[1, 2], [3, 4]]
-        }).then(setSum).catch(console.log)
+        add([[1, 2], [3, 4]], [[1, 2], [3, 4]]).then(setSum).catch(handleError)
     }, [])
+
     return (
         <div className="App">
             <header className="App-header">
-                <header>{sum}</header>
-                <img src={logo} className="App-logo" alt="logo"/>
-                <p>
-                    Edit <code>src/App.tsx</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
+                {sum}
             </header>
         </div>
     );
