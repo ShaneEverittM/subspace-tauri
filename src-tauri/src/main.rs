135e0b74b430e0c0469727b9f2a89a5198b1e9a2
ShaneEverittM <shaneeverittmurphy@gmail.com>
2021-05-13 00:44:52 -0700

    Reorganize into separate files.

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,46 +1,12 @@
 #![cfg_attr(
-    all(not(debug_assertions), target_os = "windows"),
-    windows_subsystem = "windows"
+all(not(debug_assertions), target_os = "windows"),
+windows_subsystem = "windows"
 )]
-use std::convert::TryInto;
-use std::string::ToString;
 
-use basis::Matrix;
-use serde::Serialize;
-use serde_repr::Serialize_repr;
+use basis_api::*;
 
-#[derive(Serialize_repr)]
-#[repr(u8)]
-pub enum ApiErrorKind {
-    BasisError,
-    InvalidInput,
-}
-
-#[derive(Serialize)]
-pub struct ApiError {
-    kind: ApiErrorKind,
-    msg: String,
-}
-
-// TODO: Fix once Basis changes to `thiserror`
-impl From<anyhow::Error> for ApiError {
-    fn from(e: anyhow::Error) -> Self {
-        ApiError {
-            kind: ApiErrorKind::InvalidInput,
-            msg: e.to_string(),
-        }
-    }
-}
-
-#[tauri::command]
-pub fn add(v1: Vec<Vec<i32>>, v2: Vec<Vec<i32>>) -> Result<Vec<Vec<i32>>, ApiError> {
-    Matrix::add(&v2.try_into()?, &v1.try_into()?)
-        .map(|m| (*m).clone())
-        .map_err(|e| ApiError {
-            kind: ApiErrorKind::InvalidInput,
-            msg: e.to_string(),
-        })
-}
+mod basis_api;
+mod util;
 
 fn main() {
     tauri::Builder::default()
