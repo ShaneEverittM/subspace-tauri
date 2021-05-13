362ab1545ddafcb77f7ae01eab13e7ecf644e861
ShaneEverittM <shaneeverittmurphy@gmail.com>
2021-05-13 00:44:12 -0700

    Remove unnecessary wrapper API.

--- src/api/Arithmetic.ts
+++
@@ -1,5 +0,0 @@
-import { invoke } from '@tauri-apps/api/tauri';
-
-export function add(v1: Array<Array<number>>, v2: Array<Array<number>>): Promise<Array<Array<number>>> {
-    return invoke<Array<Array<number>>>('add', {v1, v2});
-}
