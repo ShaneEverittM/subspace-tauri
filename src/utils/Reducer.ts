4e5575f8f7471061510fa8f84cf4bd549ad1cf05
ShaneEverittM <shaneeverittmurphy@gmail.com>
2021-05-31 15:21:59 -0700

    Change to switch statement to protect against new types being added breaking silently.

--- src/utils/Reducer.ts
+++ src/utils/Reducer.ts
@@ -1,31 +1,35 @@
 import { Array2d, make2d } from './Matrix';
 
 
+/**
+ * Resizes the matrix, filling with a  given value
+ */
 export type ResizeAction<T> = { type: 'resize', newSize: number, filler: T }
 
+
 /**
- * Represents the action applied to matrices held in state.
- *
- * @typeParam The value the matrix holds.
+ * Updates an individual cell to a new value
  */
 export type CellUpdateAction<T> = { type: 'cell-update', row: number, col: number, newVal: T }
 
+/**
+ * Represents the possible actions that can be applied to matrices held in state.
+ */
 export type Action<T> = ResizeAction<T> | CellUpdateAction<T>
 
 /**
- * Creates a specialized reducer function for `useReducer`.
- *
- * @typeParam The type of the element of the Matrix being edited.
+ * Creates a specialized reducer function for the `useReducer` hook.
  */
 export function makeReducer<T>(): (previous: Array2d<T>, action: Action<T>) => Array2d<T> {
     return function (previous: Array2d<T>, action: Action<T>): Array2d<T> {
-        if (action.type === 'cell-update') {
-            const {row, col, newVal} = action;
-            previous[row][col] = newVal;
-            return previous;
-        } else {
-            let {newSize, filler} = action;
-            return make2d(newSize, filler);
+        switch (action.type) {
+            case 'resize':
+                let {newSize, filler} = action;
+                return make2d(newSize, filler);
+            case 'cell-update':
+                const {row, col, newVal} = action;
+                previous[row][col] = newVal;
+                return previous;
         }
     };
 }
